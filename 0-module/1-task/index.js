a3d2dc369579652d859731939784c34ec1d7c5c4
Kokoold <skalkin-2003@mail.ru>
2022-11-01 00:13:49 +0500

    Вычисление факториала и создание функции сложения

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sum(m, n) {
-  // ваш код...
-}
+  return m+n;
+ }
+ let result = sum(1,1)
+ alert(result);
+ 
+ let result2 = sum(2,5)
+ alert(result2)
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  result = n
+  for( let i = n-1; i>1; --i){
+    result *= i;
+  }
+  
+  return result
 }
+alert(factorial(0));
+alert(factorial(1));
+alert(factorial(3));
+alert(factorial(5));

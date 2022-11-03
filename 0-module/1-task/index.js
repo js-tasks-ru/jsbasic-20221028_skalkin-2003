b6825ff626b73d073f70b11a098a9e66c96feeee
Kokoold <skalkin-2003@mail.ru>
2022-11-04 00:34:54 +0500

    Исправление функции факториала

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -2,7 +2,4 @@ function sum(m, n) {
   return m+n;
  }
  let result = sum(1,1)
- alert(result);
- 
  let result2 = sum(2,5)
- alert(result2)
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,4 @@
+
 function factorial(n) {
-  result = n
-  for( let i = n-1; i>1; --i){
-    result *= i;
-  }
-  
-  return result
+  return n ? n * factorial(n - 1) : 1;
 }
-alert(factorial(0));
-alert(factorial(1));
-alert(factorial(3));
-alert(factorial(5));
